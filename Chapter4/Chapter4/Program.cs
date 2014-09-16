2c01a6f9c6b002cab3183135187ffe885173ac9a
rbnsfr <rbnsfr@outlook.com>
2014-09-16 08:44:59 -0400

    Chapter 4
    Started

---
+++ Chapter4/Chapter4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, total;
+
+            Console.Write("Enter a value, or 999 to stop: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
